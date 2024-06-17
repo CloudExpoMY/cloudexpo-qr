68d316080f396ddd6e27b8f79a069a66a4e753c5
Liren Yeo <liren@cloudexpo.my>
2024-06-17 18:33:46 +0800

    remove express and cors

--- README.md
+++ README.md
@@ -3,9 +3,17 @@
 Turn any string into a 8-bit QR code (supported by Meta Cloud-API).
 
 Simply visit https://qr.cloudexpo.my?v=helloworld to get the QR image.
-Or, locally: http://localhost:3000/?v=12345
+
+This is written for Netlify serverless functions.
 
 ### To run locally
 
 1. `npm install`
-2. `node index.js`
+1. `npm i -g netlify-cli`
+1. `netlify functions:serve`
+1. Visit `http://localhost:9999/.netlify/functions/qr?v=YOUR_QR_STRING`
+
+
+In `netlify.toml`, we redirect `/*` to `/.netlify/functions/qr`.
+
+This way, visiting `domain.com?v=1` is equivalent of `localhost:9999/.netlify/functions/qr?v=1`.
---
+++ netlify/functions/qr/qr.js
@@ -0,0 +1,38 @@
+const QRCode = require('qrcode')
+
+const handler = async (event, context) => {
+  const { rawQuery } = event
+  const data = rawQuery.split('=')[1]
+
+  if (!data) {
+    return { statusCode: 400, body: 'No data provided' }
+  }
+
+  try {
+    const options = {
+      margin: 2,
+      width: 300,
+      height: 300,
+    }
+
+    console.log('Generating QR code...')
+    const qrCodeDataURL = await QRCode.toDataURL(data, options)
+    const imgBuffer = Buffer.from(qrCodeDataURL.split(',')[1], 'base64')
+
+    console.log('QR code generated, sending response...')
+    return {
+      statusCode: 200,
+      headers: {
+        'Content-Type': 'image/png',
+        'Content-Disposition': 'inline; filename="CloudExpo-QR.png"',
+      },
+      body: imgBuffer.toString('base64'),
+      isBase64Encoded: true,
+    }
+  } catch (err) {
+    console.error('Error generating QR code:', err)
+    return { statusCode: 500, body: 'Error generating QR code' }
+  }
+}
+
+module.exports = { handler }
